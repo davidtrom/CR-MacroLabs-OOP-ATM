58e2a02fe945c83716e7661fb17e0de29e2cfa90
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-10.local>
2019-10-26 12:55:54 -0400

    I added in the scanner Package and Console Methods

--- src/main/java/ConsoleClasses/Console.java
+++ src/main/java/ConsoleClasses/Console.java
@@ -1,4 +1,30 @@
 package ConsoleClasses;
+import java.util.Scanner;
 
 public class Console {
-}
+
+
+    public static void print(String output, Object... args) {
+        System.out.printf(output, args);
+    }
+
+    public static void println(String output, Object... args) {
+        print(output + "\n", args);
+    }
+
+    public static String getStringInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public static Integer getIntegerInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        Integer userInputInt = scanner.nextInt();
+        return userInputInt;
+    }
+
+
+    }
